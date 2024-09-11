69e9e6210184244c94b3186c5476f80823a4294f
Chonlatharn88 <Chonlatharnkkck@gmail.com>
2024-09-11 11:37:32 +0700

    Add files via upload

---
+++ phpbasic/function26.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Writing PHP function</title>
+</head>
+<body>
+    <?php
+    /* Defining a PHP Function */
+    function writeMessage()
+    {
+        echo "You are really a nice person, Have a nice time";
+    }
+    /* Calling a PHP function*/
+    writeMessage();
+    ?>
+    
+</body>
+</html>
